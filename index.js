97b21bb5d719d24667ba40280d4d15b8e3c02f68
luanrodriguespigosso <luan_naul90@hotmail.com>
2020-02-29 15:22:53 -0300

    Padronizando código

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Subir o servidor
-const customExpress = require('./config/customExpress')
-const conexao = require('./infraestrutura/conexao')
+const customExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
 
 conexao.connect(erro => {
     if (erro) {
-        console.log(erro)
+        console.log(erro);
     } else {
-        console.log('Conectado ao db com sucesso!')
+        console.log('Conectado ao db com sucesso!');
 
         const app = customExpress();
 

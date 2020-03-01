f5c567a78e5a7050b9b832e943117278b4ce4deb
luanrodriguespigosso <luan_naul90@hotmail.com>
2020-03-01 14:12:20 -0300

    Outros verbos HTTP-GET

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,12 +2,20 @@
 const Atendimento = require('../models/atendimentos');
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) =>
-        res.send('Pagina de atendimentos'));
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res);
+    });
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+
+        Atendimento.buscaPorId(id, res);
+    });
+
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
 
         Atendimento.adiciona(atendimento, res)
-    })
+    });
 }
